3a084b3a1dbcb7c630ceb90af26aeed35a407a2f
ajcltm <ajcltm@gmail.com>
2022-07-12 00:55:36 +0900

    2022.7.12. 00:54

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='naverLand_v2',
+      version='0.1',
+      url='https://github.com/ajcltm/naverLand_v2',
+      license='jnu',
+      author='ajcltm',
+      author_email='ajcltm@gmail.com',
+      description='',
+      packages=find_packages(exclude=['test']),
+      zip_safe=False,
+      setup_requires=['requests>=1.0'],
+      test_suite='test.test_guScraper')
